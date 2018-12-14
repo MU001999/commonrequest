7dd8f4eec00780814c7be54e97484e6da67ef10d
mu001999 <mu001999@outlook.com>
2018-12-14 23:12:42 +0800

    update crq.hpp

--- crq.hpp
+++ crq.hpp
@@ -27,12 +27,14 @@ namespace crq
         ::std::string charset;
         ::std::string text;
 
+        ::std::string raw;
+
 
         Response() {}
 
         Response(::std::string &data)
         {
-
+            raw = data;
         }
 
         Response(Response &&res) noexcept = default;
@@ -63,22 +65,25 @@ namespace crq
     {
     private:
 
-        static ::std::string gen_req(::std::string url);
+        static ::std::string gen_req(::std::string url)
+        {
+
+        }
 
         static Response request(const ::std::string url, const ::std::string method)
         {
             ::std::string response_msg;
 
             // init server_addr
-            addrinfo hints = {}, *addrs;
+            addrinfo hints = {}, *result;
             hints.ai_family = AF_INET;
             hints.ai_socktype = SOCK_STREAM;
             hints.ai_protocol = IPPROTO_TCP;
-            getaddrinfo(url.c_str(), "80", &hints, &addrs);
+            getaddrinfo(url.c_str(), "80", &hints, &result);
 
             // connect to server
             auto socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-            for (auto addr = addrs; addr != NULL; addr = addr->ai_next)
+            for (auto addr = result; addr != NULL; addr = addr->ai_next)
             {
                 if (connect(socket_fd, addr->ai_addr, addr->ai_addrlen) == 0)
                 {
@@ -86,7 +91,7 @@ namespace crq
                 }
             }
 
-            // send request message
+            // send request message to server
             {
                 ::std::string request_msg = " HTTP/1.1\r\n"
                     "Connection: keep-alive\r\n"
@@ -94,9 +99,21 @@ namespace crq
                     "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\n"
                     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0\r\n";
                 request_msg = method + gen_req(url) + request_msg;
+
+                send(socket_fd, request_msg.c_str(), request_msg.length(), 0);
             }
 
-            freeaddrinfo(addrs);
+            // receive response message from server
+            {
+                char tmp[4096] = { 0 };
+
+                while (recv(socket_fd, tmp, 4096, 0) > 0)
+                {
+                    response_msg += tmp;
+                }
+            }
+
+            freeaddrinfo(result);
 
             close(socket_fd);
 
