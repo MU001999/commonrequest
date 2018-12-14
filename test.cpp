c017f033def3ce0bf7a5a49ae20f0e0c209b157c
mu001999 <mu001999@outlook.com>
2018-12-14 23:12:49 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
 #include "crq.hpp"
 
+#include <iostream>
+
+
+using namespace std;
+
 
 int main(int argc, char *argv[])
 {
+    auto res = crq::Request::get("www.baidu.com");
+
+    cout << res.raw << endl;
+
     return 0;
 }
