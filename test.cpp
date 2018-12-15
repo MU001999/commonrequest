82909c9088f9092e889f7fa83fd7c5141b24a3a1
mu001999 <mu001999@outlook.com>
2018-12-15 17:42:40 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,9 +7,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    auto res = crq::Request::get("www.baidu.com");
+    auto res = crq::Request::get("http://mil.sohu.com/");
+    //auto res = crq::Request::get("www.baidu.com");
+    //if (res.status_code == 302) res = crq::Request::get(res.headers["Location"], {
+    //    {"Cookie", res.headers["Set-Cookie"]},
+    //    });
 
-    cout << res.raw << endl;
+    cout << res.status_code << endl;
+    cout << res.reason << endl;
+    cout << res.body << endl;
 
     return 0;
 }
