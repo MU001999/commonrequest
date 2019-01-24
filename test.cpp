32afa540ce09c3551a83f72902d35339ff9100d3
mu001999 <mu001999@outlook.com>
2019-01-24 16:05:04 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    auto res = crq::Request::get("http://mil.sohu.com/");
+    auto res = crq::get("http://mil.sohu.com/");
     //auto res = crq::Request::get("www.baidu.com");
     //if (res.status_code == 302) res = crq::Request::get(res.headers["Location"], {
     //    {"Cookie", res.headers["Set-Cookie"]},
